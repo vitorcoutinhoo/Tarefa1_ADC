84257f8bb072b4fa941d84e1ba694bc0c4168ab1
Vitor <vitorcooutinho@gmail.com>
2025-02-12 18:21:27 -0300

    feat: Add pwm for red and blue led

--- adc.c
+++ adc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
+#include "hardware/pwm.h"
 
 #define VRX_PIN 27
 #define VRY_PIN 26
@@ -15,17 +16,31 @@ uint32_t volatile last_time = 0;
 void green_led_handler() {
     uint32_t current_time = to_us_since_boot(get_absolute_time());
 
-    // Debouncing de 350ms
-    if(current_time - last_time > 350000) {
+    // Debouncing de 250ms
+    if(current_time - last_time > 250000) {
         last_time = current_time;
         gpio_put(GREEN_PIN, !gpio_get(GREEN_PIN));
     }
         
+}
 
+uint pwm_init_gpio(uint gpio, uint wrap) {
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(gpio);
+    
+    pwm_set_wrap(slice, wrap);
+    pwm_set_enabled(slice,true);
+    
+    return slice;
 }
 
 int main() {
     stdio_init_all();
+    uint wrap = 4095;
+
+    uint blue_slice = pwm_init_gpio(BLUE_PIN, wrap);
+    uint red_slice = pwm_init_gpio(RED_PIN, wrap);
+    
     adc_init();
     adc_gpio_init(VRX_PIN);
     adc_gpio_init(VRY_PIN);
@@ -37,12 +52,6 @@ int main() {
     gpio_init(GREEN_PIN);
     gpio_set_dir(GREEN_PIN, GPIO_OUT);
 
-    gpio_init(BLUE_PIN);
-    gpio_set_dir(BLUE_PIN, GPIO_OUT);
-
-    gpio_init(RED_PIN);
-    gpio_set_dir(RED_PIN, GPIO_OUT);
-
     gpio_set_irq_enabled_with_callback(SW_PIN, GPIO_IRQ_EDGE_FALL, true, &green_led_handler);
     
     while (true) {
@@ -51,6 +60,9 @@ int main() {
 
         adc_select_input(0);
         uint16_t vry_value = adc_read();
+
+        pwm_set_gpio_level(BLUE_PIN, (vry_value * wrap) / 4095);
+        pwm_set_gpio_level(RED_PIN, (vrx_value * wrap) / 4095);
         
         printf("VRX: %u, VRY: %u\n", vrx_value, vry_value);
         
